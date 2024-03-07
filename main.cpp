1a181af14fdd0a58ce1a89019c2d86328d2c461b
Nerdeee <48033320+Nerdeee@users.noreply.github.com>
2024-03-07 01:58:07 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+bool isAuthorized(string password, string securitylevel) {
+	// make sure user password and sec level match whats in the file
+}
+
+void randomizeFileSecurity() {
+	// give the file names in the txt document of dummy files 
+}
+
+int main() {
+	string securitylevel = "";
+	string password = "";
+	cout << "Enter your security level (Enter 'public', 'private', 'secret', or 'topsecret')" << endl;
+	cin >> securitylevel;
+	cout << "Enter the password for your security level" << endl;
+	cin >> password;
+	
+	
+}
